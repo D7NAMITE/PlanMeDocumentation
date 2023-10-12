61cd69ebc188b3a0ac10a91357aa70e9f32d3477
PhumrapeeC <phumrapeegenius@gmail.com>
2023-10-13 04:43:12 +0700

    Set path for home page.

--- planme/users/urls.py
+++ planme/users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("~update/", view=user_update_view, name="update"),
     path("<str:username>/", view=user_detail_view, name="detail"),
     path('login/', views.login, name="login"),
+    path('', views.home, name="home"),
 ]
