04c22caf1b7d9dd918aadea8f2a8a080161a29b1
PhumrapeeC <phumrapeegenius@gmail.com>
2023-10-13 05:09:53 +0700

    Set path for logout

--- planme/users/urls.py
+++ planme/users/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login/', views.login, name="login"),
     path('', views.home, name="home"),
     path('postlogin/', views.post_login),
+    path('logout/', views.logout, name="logout"),
 ]
