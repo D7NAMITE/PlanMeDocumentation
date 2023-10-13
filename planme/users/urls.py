9e648d95721374e8babd65dd50372de771d6a0a1
PhumrapeeC <phumrapeegenius@gmail.com>
2023-10-13 17:52:11 +0700

    Remove unuse path in urlpatterns.

--- planme/users/urls.py
+++ planme/users/urls.py
@@ -7,10 +7,4 @@ urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<str:username>/", view=user_detail_view, name="detail"),
-    path('login/', views.login, name="login"),
-    path('', views.home, name="home"),
-    path('postlogin/', views.post_login),
-    path('logout/', views.logout, name="logout"),
-    path('signup/', views.signup, name="signup"),
-    path('postsignup/', views.post_signup),
 ]
