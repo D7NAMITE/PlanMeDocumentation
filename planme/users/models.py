fb5811f5ff216b921a119957064abc5e98875ed2
PhumrapeeC <phumrapeegenius@gmail.com>
2023-10-15 12:36:25 +0700

    Add SubTask class.

--- planme/users/models.py
+++ planme/users/models.py
@@ -30,3 +30,14 @@ class Task(models.Model):
     due_date = models.DateField()
     status = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+
+class SubTask(models.Model):
+    subtask_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    summarized_text = models.TextField()
+    bullet_text = models.TextField()
+    due_date = models.DateField()
+    status = models.CharField(max_length=255)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='subtasks')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
